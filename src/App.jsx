74144156ebace0d706d16d9362c5b597fe413e1b
Vishal Sharma <sharmavishal.193@gmail.com>
2025-06-01 00:15:45 -0700

    Initial commit: self-hosted Spline scene

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <main style={{ width: '100vw', height: '100vh' }}>
+      <Spline scene="/genkub_greeting_robot.spline" />
+    </main>
+  );
+}
